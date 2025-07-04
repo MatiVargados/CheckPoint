18df2898d23ffd307349d8d4d4463f4e88196f59
Keruza3 <JUANIGNACIO2520@gmail.com>
2025-07-03 23:50:05 -0300

    segundo endpoint + arreglos del pkg

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,12 @@ const app = express();
 app.use(cors()); // Middleware CORS basico que permite todas las solicitudes
 
 
+/*
 //RUTAS//
 app.get("/", (req, res) => {
     res.send("hola mundo");
 });
+*/
 
 //CRUD -> create(POST) - Read(GET) - Update(PUT) - Delete(DELETE)
 //1. primer endpoint GET -> traer todos los productos
@@ -40,6 +42,36 @@ app.get("/productos", async (req, res) => {
     }
 });
 app.listen(port, () =>{
-    console.log(`Servidor corriendo en el puerto ${port}`);
     console.log(`link: http://localhost:${port}`);
+});
+
+// 2. Segundo endpoint GET by id -> Traer producto por su id
+// TODO, hacer middleware validateId
+app.get("/products/:id", async (req, res) => {
+  try {
+    // let id = req.params.id
+    let { id } = req.params;
+
+    // Consulta no optima, porque permite la inyeccion SQL
+    let sql = "SELECT * FROM productos where id = ?";
+
+    let [rows] = await connection.query(sql, [id]);
+
+    // Verificamos si se encontro el producto
+    if (rows.length === 0) {
+      return res.status(404).json({
+        error: `No se encontro el producto con id: ${id}`
+      });
+    }
+
+    res.status(200).json({
+      payload: rows
+    });
+  } catch (error) {
+    console.error(`Error obteniendo producto con id ${id}`, error.message);
+
+    res.status(500).json({
+      error: "Error interno al obtener un producto por id"
+    });
+  }
 });
